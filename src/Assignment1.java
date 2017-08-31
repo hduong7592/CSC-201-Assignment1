ae4eb92586423dc9bcfc014075037250f87ee2fd
Hieu Duong <hieuduong08@gmail.com>
2017-08-31 02:10:53 -0400

    Initial commit

---
+++ src/Assignment1.java
@@ -0,0 +1,23 @@
+/*
+Hieu Duong
+CSC 201-004N
+Assignment 1
+Problem 1.6:
+(Summation of a series) Write a program that displays the result of
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+
+08/30/2017
+*/
+
+public class Assignment1 {
+    public static void main(String[] args)
+    {
+        System.out.println("This program will calculate the summation of a series.");
+
+        //Summation of a series
+        int Sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+        //Print out the result
+        System.out.println("Summation of: 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 is:  "+Sum);
+    }
+}
